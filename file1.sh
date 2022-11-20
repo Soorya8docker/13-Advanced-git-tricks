24f9d468929fdc921f67941a5a2e22c9792d3093
Soorya8docker <ramanathansoorya56@gmail.com>
2022-11-20 16:10:53 +0530

    nice

--- file1.sh
+++ file1.sh
@@ -1,6 +1,18 @@
 #This is my first commit
 echo "HELLO World"
+git add .
+git commit -am "hi mom"
 
 #This is my second commit
-echo "My sencond comit"
+echo "My sencond comit using 'commit -am'"
+git commit -am "that was easy!"
 
+#This is my third commit
+echo "My third commit using 'alias' method"
+git config --global alias.ac "commit -am"
+git ac "noice!"
+
+#This is my forth commit
+echo "To clear or change the spelling mistake done in my previos commit"
+git commit --amend -m "nice"
+#The previous commit message is changed
